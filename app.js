e8aebbd6d51e965dd4867010c02b084d3dde38a0
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2025-03-31 17:03:38 +0530

    add origin in cors.

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const limiter = RateLimit({
 app.use(limiter);
 app.use(helmet());
 app.use(compression());
-app.use(cors({origin: true, credentials: true}));
+app.use(cors({origin: 'https://void-storage-frontend.vercel.app', credentials: true}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
