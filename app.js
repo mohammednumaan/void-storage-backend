aa74bbedea236fc397176ead846babbd8b43ff62
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2025-03-31 19:22:25 +0530

    update cookie config.

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(
       secure: true,
       domain: 'void-storage-frontend.vercel.app',
       path: '/',
+      partitioned: true
     },  
     secret: process.env.SESSION_SECRET,
     resave: false,
