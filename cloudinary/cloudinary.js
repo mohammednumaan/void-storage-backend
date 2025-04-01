b4b27c373ee3c35fca97649a2e3daa244cb12dee
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2025-04-01 11:41:49 +0530

    fix: modify cloudinary type option for file upload.

--- cloudinary/cloudinary.js
+++ cloudinary/cloudinary.js
@@ -68,7 +68,7 @@ class CloudinaryInterface{
 
     static async uploadFileCloudinary(folderPath, dataUri, isText){
         try{        
-            const uploadedFile = await cloudinary.uploader.upload(dataUri, {asset_folder: folderPath, use_asset_folder_as_public_id_prefix: true, resource_type: isText ? 'raw' : 'image'})
+            const uploadedFile = await cloudinary.uploader.upload(dataUri, {asset_folder: folderPath, use_asset_folder_as_public_id_prefix: true, resource_type: isText ? 'raw' : 'auto'})
             return uploadedFile;
         } catch(error){
             console.log(error)
